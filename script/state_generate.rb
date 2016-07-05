b4649900aa3cf838b8eab9c59d35b425b3018c0b
Jonathan Arp <jarp@nd.edu>
2016-07-04 22:26:05 -0400

    added to script

--- script/state_generate.rb
+++ script/state_generate.rb
@@ -1,5 +1,7 @@
 require 'csv'
-
 CSV.foreach("script/states.csv") do |row|
   Plate.create!(state: row[0], code: row[1])
 end
+
+GameType.create!(name: "Collaborate")
+GameType.create!(name: "Combat")
