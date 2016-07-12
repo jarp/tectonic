fb4fb99649ee800850b52fde1ff87063ea4bed8e
Jonathan Arp <jarp@nd.edu>
2016-07-11 20:10:50 -0400

    service

---
+++ app/services/location_service.rb
@@ -0,0 +1,24 @@
+class LocationService
+
+    def self.lookup(state)
+      puts  "[location srv] fetching target location from google"
+      uri = URI("http://maps.googleapis.com/maps/api/geocode/json?address=#{state}")
+      http = Net::HTTP.new(uri.host, uri.port)
+      request = Net::HTTP::Get.new(uri.request_uri)
+      res = http.request(request)
+      return JSON.parse(res.body)["results"][0]["geometry"]["location"]
+    end
+
+    def self.distance(from,state)
+        endpoint = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=#{from.gsub('|',',')}&destinations=State of #{state}&key=#{ENV['TECTONIC_API']}"
+        puts "[location srv] endpoint is: #{endpoint}"
+        uri = URI(endpoint)
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE # You should use VERIFY_PEER in production
+        request = Net::HTTP::Get.new(uri.request_uri)
+        res = http.request(request)
+        puts "[location srv] found distance matrix: #{JSON.parse(res.body)}"
+        return JSON.parse(res.body)["rows"][0]["elements"][0]["distance"]["text"]
+    end
+end
