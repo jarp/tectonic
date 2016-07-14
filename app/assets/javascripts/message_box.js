8f7f8faad25536dcbf92dd42bbcf6e90144a81f1
Jonathan Arp <jarp@nd.edu>
2016-07-14 16:56:41 -0400

    amazing stuff

--- app/assets/javascripts/message_box.js
+++ app/assets/javascripts/message_box.js
@@ -12,18 +12,19 @@ $(document).ready(function(){
   MessageBox = {
 
     set: function(msg, format){
-
+      $('#message-box').css('bottom', '-150px');
+      /*
       $('html, body').animate({
         scrollTop: $('html').offset().top
         },
       1000);
-
-
+      */
+      
       if ( format == 'undefined'){
           format = 'normal';
         }
 
-      this.log("set message as " + format)
+      this.log("set message '"  + msg + "' as " + format)
 
       if ( format == 'html'){
         $('#message-box #message-text').html(msg);
@@ -31,19 +32,22 @@ $(document).ready(function(){
       else {
         $('#message-box #message-text').text(msg);
       }
-
-      $('#message-box').fadeIn();
+      $('#message-box').show();
+      $("#message-box").animate({ "bottom": "+=100px" }, 900);
 
       setTimeout("MessageBox.clear()", Tectonic.getTimer());
       },
 
     clear: function(){
-      $('#message-box').fadeOut();
+      $('#message-box').fadeOut('slow',function(){
+        $('#message-box').css('bottom', '-150px');
+      });
       $('#message-box #message-text').text("");
+
       },
 
     log: function(m){
-      // console.log(m);
+       console.log(m);
       }
   }
 MessageBox.clear();
