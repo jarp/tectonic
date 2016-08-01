4cfd4f5fc09b2b549fa3e4ab69195ad12f717db9
Jonathan Arp <jarp@nd.edu>
2016-08-01 01:25:51 -0400

    completed

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -1,6 +1,6 @@
 class Api::GamesController < GamesController
   def index
-    @games = @active_player.games.active
+    @games = params[:status] == 'completed' ? @active_player.games.completed : @active_player.games.active
   end
   def show
   end
