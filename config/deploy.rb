8f9fd63f2d6c6e57aa9c833a0152791effb1a64a
Jonathan Arp <jarp@nd.edu>
2016-12-01 16:13:12 -0500

    gemset

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,8 +21,8 @@ set :repo_url, 'git@github.com:ndwebgroup/find-app.git'
  set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
  set :keep_releases, 5
  set :rvm_type, :user
- set :rvm_ruby_version,'ruby-2.2.4' # Edit this if you are using MRI Ruby
- #set :rvm_ruby_version, '2.2.4@my_gemset_name'
+ #set :rvm_ruby_version,'ruby-2.2.4' # Edit this if you are using MRI Ruby
+ set :rvm_ruby_version, '2.2.4@default'
 
  set :puma_rackup, -> { File.join(current_path, 'config.ru') }
  set :puma_state, "#{shared_path}/tmp/pids/puma.state"
